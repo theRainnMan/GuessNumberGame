438e780c502fc0bc8a8d42f0ed2845f647751b87
George (Zhiheng Li) <2433535844@qq.com>
2017-12-07 15:17:40 -0500

    The guessing number game used Angular JS
    please see README file

---
+++ README.md
@@ -0,0 +1,5 @@
+Run the program install npm and run: " node server.js "
+
+access http://localhost:3000/game.html
+
+click start game to start
---
+++ public/js/game.js
@@ -0,0 +1,42 @@
+/**
+ * Created by George on 2017/12/7.
+ */
+var app = angular.module('mainApp', []);
+
+app.controller("mainCtrl", ["$scope", function ($scope) {
+    $scope.lowStatus = false;
+    $scope.highStatus = false;
+    $scope.winStatus = false;
+    $scope.currentMin = 0;
+    $scope.currentMax = 100;
+    $scope.GuessNumber = function (userGuess) {
+        $scope.rr = userGuess;
+        if(userGuess === $scope.randomNumber){
+            $scope.winStatus = true;
+            console.log("win");
+            $scope.highStatus = false;
+            $scope.lowStatus = false;
+        }
+        else if(userGuess < $scope.randomNumber){
+            $scope.highStatus = false;
+            $scope.lowStatus = true
+            $scope.currentMin = userGuess;
+            console.log("small");
+        }
+        else {
+            $scope.lowStatus = false;
+            $scope.highStatus = true
+            $scope.currentMax = userGuess;
+            console.log("large");
+        }
+    }
+
+    $scope.generateNum = function () {
+        $scope.randomNumber = Math.floor(Math.random() * 100) + 1;
+        $scope.lowStatus = false;
+        $scope.highStatus = false;
+        $scope.winStatus = false;
+        console.log($scope.randomNumber);
+    }
+
+}]);
